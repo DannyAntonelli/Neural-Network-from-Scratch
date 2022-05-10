71c78174c75aa9e6fd53afb0c38378f210848b62
DannyAntonelli <dannyantonelli2002@gmail.com>
2022-05-10 09:25:34 +0200

    Add layers

---
+++ neuralnetwork/layers.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+from .activations import Activation, Linear
+
+
+class Layer(ABC):
+    '''
+    Protocol implemented by neural network layers
+    '''
+
+    @abstractmethod
+    def forward_propagate(self, input: np.ndarray) -> np.ndarray:
+        ...
+
+    @abstractmethod
+    def backward_propagate(self, output_error: np.ndarray, learning_rate: float) -> np.ndarray:
+        ...
+
+
+class Dense(Layer):
+    def __init__(self, input_size: int, units: int, activation: Activation=Linear()):
+        self._weights = np.random.randn(input_size, units) * np.sqrt(2. / input_size)
+        self._bias = np.zeros((1, units))
+        self._activation = activation
+
+    def forward_propagate(self, input: np.ndarray) -> np.ndarray:
+        self._input = input
+        self._output = np.dot(input, self._weights) + self._bias
+        return self._activation(self._output)
+
+    def backward_propagate(self, output_gradient: np.ndarray, learning_rate: float) -> np.ndarray:
+        output_gradient *= self._activation.gradient(self._output)
+        self._weights -= learning_rate * np.dot(self._input.T, output_gradient)
+        self._bias -= learning_rate * output_gradient
+        return np.dot(output_gradient, self._weights.T)
