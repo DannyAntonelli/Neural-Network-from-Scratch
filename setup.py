1147780b46d3f8b2aa13a838294798794e952c2f
DannyAntonelli <dannyantonelli2002@gmail.com>
2022-05-10 09:25:17 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neuralnetwork',
+    version="1.0.0",
+    author='DannyAntonelli',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    python_requires='>=3.10'
+)
