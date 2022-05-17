1e6c7ce3b46bcf45a283204da5a6ba8a792827f5
DannyAntonelli <dannyantonelli2002@gmail.com>
2022-05-17 16:40:11 +0200

    Add dense layer tests

---
+++ neuralnetwork/tests/test_layers.py
@@ -0,0 +1,26 @@
+import numpy as np
+import unittest
+
+from neuralnetwork.layers import Dense
+
+
+class TestDense(unittest.TestCase):
+    def setUp(self):
+        self.input_size = 30
+        self.units = 100
+        self.layer = Dense(self.input_size, self.units)
+
+    def test_forward_shape(self):
+        input = np.random.rand(1, self.input_size)
+        self.assertEqual(self.layer.forward_propagate(input).shape, (1, self.units))
+
+    def test_backward_shape(self):
+        input = np.random.rand(1, self.input_size)
+        self.layer.forward_propagate(input)
+        output_gradient = np.random.rand(1, self.units)
+        learning_rate = 0.1
+        self.assertEqual(self.layer.backward_propagate(output_gradient, learning_rate).shape, (1, self.input_size))
+
+
+if __name__ == "__main__":
+    unittest.main()
