15085c1cf4a5eb0953a8dd12abd68a87e8eff564
DannyAntonelli <dannyantonelli2002@gmail.com>
2022-05-19 08:17:37 +0200

    Add missing comma

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="neuralnetwork",
     version="1.0.0",
-    description="Neural Network from scratch"
+    description="Neural Network from scratch",
     author="Danny Antonelli",
     packages=find_packages(),
     install_requires=["numpy"],
