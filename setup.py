50bceead976f3c45658bf2506940d1cc0ea47be4
DannyAntonelli <dannyantonelli2002@gmail.com>
2022-05-19 08:15:51 +0200

    Add description to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="neuralnetwork",
     version="1.0.0",
-    author="DannyAntonelli",
+    description="Neural Network from scratch"
+    author="Danny Antonelli",
     packages=find_packages(),
     install_requires=["numpy"],
     python_requires=">=3.10"
